2a4534e84c8e0595b174faa18bd04d59984e4c7d
AYUSHIPATEL123 <131158716+AYUSHIPATEL123@users.noreply.github.com>
2025-09-23 19:54:38 +0530

    mod 2

--- manapi/wait-for-db.sh
+++ manapi/wait-for-db.sh
@@ -5,7 +5,7 @@ set -e
 host='db'
 cmd="$@"
 
-echo"⏳Waiting for MySQL at $host..."
+echo "⏳Waiting for MySQL at $host..."
 
 until mysql -h "$host" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e 'SELECT 1' &> /dev/null; do
     >&2 echo "MySQL is unavailable - sleeping" 
